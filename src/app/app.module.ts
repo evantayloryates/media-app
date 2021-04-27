74f96eb5169e4b9d3ed50c7a3d9414173e08e46e
Taylor Yates <evantayloryates@gmail.com>
2021-04-26 17:32:45 -0700

    Import new components
    New components: Header, Footer, Home, PageNotFound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
