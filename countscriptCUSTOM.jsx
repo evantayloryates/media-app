eb883b9770e52699cd1d4494f46c6f5a641f10ce
Evan Taylor Yates <evantayloryates@gmail.com>
2022-06-01 19:09:06 -0700

    Update countscriptCUSTOM.jsx

--- countscriptCUSTOM.jsx
+++ countscriptCUSTOM.jsx
@@ -16,7 +16,18 @@
     "user": "User Icon",
   }
 
-  var mediaAssets = NX.media_assets || ['hello'];
+  var tempAssets = [
+    { 'layerName': 'Brand Avatar',   'filename': 'avatar.jpeg'   },
+    { 'layerName': 'Brand Avatar 2', 'filename': 'avatar2.jpeg'  },
+    { 'layerName': 'Bookmark Icon',  'filename': 'bookmark.png'  },
+    { 'layerName': 'Comment Icon',   'filename': 'comment.png'   },
+    { 'layerName': 'Like Icon',      'filename': 'like.png'      },
+    { 'layerName': 'Main Media',     'filename': 'video.mp4'     },
+    { 'layerName': 'Profile BG',     'filename': 'profilebg.png' },
+    { 'layerName': 'User Icon',      'filename': 'user.png'      },
+  ]
+
+  var mediaAssets = NX.media_assets || tempAssets;
   
   mediaAssets.forEach(
     function (asset) {
